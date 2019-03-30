58b068f4a2db85805aed18a0b0466a0deb0f3a63
jaygroover1 <jaygroover@gmail.com>
2019-03-29 23:54:07 -0400

    1st update

---
+++ assets/javascript/app.js
@@ -0,0 +1,66 @@
+$(document).ready(function(){
+
+    //This is games intro...
+    //hides game play, plays intro and shows game plays features.........
+    $("#game").hide();
+    setTimeout(function(){ $("body").css("background-image","url(assets/images/avengers.png)"); }, 27000);
+    setTimeout(function(){ $("#game").show(); }, 30000);
+
+
+    //$("button").on("click", function(){
+
+		//$("#game").hide();
+	
+		
+        
+       
+	//});
+    
+    //old code for a click option may still use.....
+   // $( document ).keypress(function() {
+    //$("#game").show();
+      //});
+
+
+
+      // Game key.......
+     //var queryURL ="https://api.giphy.com/v1/gifs/search?q=" + movie +"&api_key=qMx31QXL9WMDmHU2rsENRoaZvQ1GbXME&=10";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
+        
+
+
+
+
+
+
+
+
+
+
